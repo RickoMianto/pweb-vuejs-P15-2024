ee0328b0500068f1899b006d2aa4a4e9d85c3c3f
RickoMianto <saputraricko0635@gmail.com>
2024-12-02 16:42:13 +0700

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',                // Jika menggunakan file HTML di root
+    './src/**/*.{vue,js,ts,jsx,tsx}', // File di folder src dengan ekstensi terkait
+  ],
   theme: {
     extend: {},
   },
