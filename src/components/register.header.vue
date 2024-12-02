e652fa154b8e53f574e3e3e4b780ea780629fea0
RickoMianto <saputraricko0635@gmail.com>
2024-12-02 16:09:26 +0700

    commit

---
+++ src/components/register.header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="entry">
+      <h1>Hello!</h1>
+      <p>Pembuatan akun baru</p>
+    </header>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'RegisterHeader',
+  };
+  </script>
